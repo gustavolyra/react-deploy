c12dc0e09b49342c645d279cf53902c0c33f6b4f
gustavolyra <32414544+gustavolyra@users.noreply.github.com>
2020-08-13 23:13:20 -0300

    change class to function

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,48 +1,55 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Description from './information/Description';
 
-export default class Body extends Component {
-  render() {
-    const name = 'Teste';
-    const record = 100;
-    const lastJoke = 20;
-    return (
-      <div>
-        <div className="row">
-          <div className="col xl3 l4 m6 s12">
-            <Description
-              name={name}
-              record={record}
-              lastJoke={lastJoke}
-              onClick={this.handleClick}
-            />
-          </div>
-          <div className="col xl3 l4 m6 s12">
-            <Description
-              name={name}
-              record={record}
-              lastJoke={lastJoke}
-              onClick={this.handleClick}
-            />
-          </div>
-          <div className="col xl3 l4 m6 s12">
-            <Description
-              name={name}
-              record={record}
-              lastJoke={lastJoke}
-              onClick={this.handleClick}
-            />
-          </div>
-          <div className="col xl3 l4 m6 s12">
-            <Description
-              name={name}
-              record={record}
-              lastJoke={lastJoke}
-              onClick={this.handleClick}
-            />
-          </div>
+export default function Body(props) {
+  const handleClick = (event, id) => {
+    console.log(id);
+    console.log(event);
+  };
+
+  const name = 'Teste';
+  const record = 100;
+  const lastJoke = 20;
+  return (
+    <div>
+      <div className="row">
+        <div className="col xl3 l4 m6 s12">
+          <Description
+            id={0}
+            name={name}
+            record={record}
+            lastJoke={lastJoke}
+            onReset={handleClick}
+          />
+        </div>
+        <div className="col xl3 l4 m6 s12">
+          <Description
+            id={1}
+            name={name}
+            record={record}
+            lastJoke={lastJoke}
+            onReset={handleClick}
+          />
+        </div>
+        <div className="col xl3 l4 m6 s12">
+          <Description
+            id={2}
+            name={name}
+            record={record}
+            lastJoke={lastJoke}
+            onReset={handleClick}
+          />
+        </div>
+        <div className="col xl3 l4 m6 s12">
+          <Description
+            id={3}
+            name={name}
+            record={record}
+            lastJoke={lastJoke}
+            onReset={handleClick}
+          />
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
--- src/components/body/information/Description.js
+++ src/components/body/information/Description.js
@@ -1,23 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import css from './info.module.css';
 
-export default class Description extends Component {
-  render() {
-    return (
-      <div className={css.description}>
-        <h4>{this.props.name}</h4>
-        <img
-          src="https://avatars3.githubusercontent.com/u/32414544?s=460&u=d5d92eb269676f9fa4fe4d9aa247662a6faa5b92&v=4"
-          alt="nome"
-        />
-        <p>
-          Estamos há {this.props.lastJoke} dias sem piadas ruim. Nosso recorde é
-          de {this.props.record} dias.
-        </p>
-        <button className="waves-effect waves-light btn-large">
-          Zerar Contagem
-        </button>
-      </div>
-    );
-  }
+export default function Description(props) {
+  const handleClick = (event) => {
+    props.onReset(event, props.id);
+  };
+  return (
+    <div className={css.description}>
+      <h4>{props.name}</h4>
+      <img
+        src="https://avatars3.githubusercontent.com/u/32414544?s=460&u=d5d92eb269676f9fa4fe4d9aa247662a6faa5b92&v=4"
+        alt="nome"
+      />
+      <p>
+        Estamos há {props.lastJoke} dias sem piadas ruim. Nosso recorde é de{' '}
+        {props.record} dias.
+      </p>
+      <button
+        className="waves-effect waves-light btn-large"
+        onClick={handleClick}
+      >
+        Zerar Contagem
+      </button>
+    </div>
+  );
 }
