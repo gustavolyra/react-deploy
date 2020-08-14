07701166f955c74483a322307094c56c3a3c2575
gustavolyra <gustavo.c.lyra@live.com>
2020-08-14 13:51:27 -0300

    send

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './components/header/Header';
 import Body from './components/body/Body';
+const axios = require('axios').default;
 
 export default class App extends Component {
   constructor() {
@@ -12,15 +13,35 @@ export default class App extends Component {
     };
   }
 
+  //get with header
+  //   async getDataAxios(){
+  //     const response =
+  //       await axios.get("https://dog.ceo/api/breeds/list/all",
+  //         { headers: {'Content-Type': 'application/json'}}
+  //       )
+  //     console.log(response.data)
+  // }
+
+  // const response = await axios.post(
+  //   'https://example.com',
+  //   { example: 'data' },
+  //   { headers: { 'Content-Type': 'application/json' } }
+  // )
+  // console.log(response.data)
+
   async componentDidMount() {
-    console.log('Hi there');
-    const res = await fetch('http://localhost:3001/ecosolys');
-    const json = await res.json();
+    const res = await axios.get('http://localhost:3001/ecosolys');
+    const json = res.data;
     console.log(json);
     this.setState({
       allJokers: json,
     });
   }
+
+  handleReset = (id) => {
+    console.log('RESET');
+    console.log(id);
+  };
 
   render() {
     return (
--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import Description from './information/Description';
+const axios = require('axios').default;
 
-export default function Body({ users }) {
-  const handleClick = (event, id) => {
+export default function Body({ users, handleReset }) {
+  const handleClick = async (event, id) => {
     console.log(id);
+    const response = await axios.post(
+      'http://localhost:3001/ecosolys/resetJoker/' + id,
+      {
+        firstName: 'Fred',
+        lastName: 'Flintstone',
+      }
+    );
+    console.log(response);
+    //handleReset(id);
   };
 
   return (
@@ -11,7 +21,7 @@ export default function Body({ users }) {
       {users.map((user) => {
         const { id, name, lastJoke, record, avatar } = user;
         return (
-          <div className="col xl3 l4 m6 s12">
+          <div key={id} className="col xl3 l4 m6 s12">
             <Description
               id={id}
               name={name}
