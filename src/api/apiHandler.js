7d3dc709706c8b5a22ed2363392a009fe22f29b2
gustavolyra <gustavo.c.lyra@live.com>
2020-08-18 14:54:19 -0300

    backend heroku

--- src/api/apiHandler.js
+++ src/api/apiHandler.js
@@ -1,17 +1,20 @@
 import axios from 'axios';
 
 async function getApi() {
-  const res = await axios.get('http://localhost:5000/ecosolys');
+  const res = await axios.get('https://badjokes-server.herokuapp.com/ecosolys');
   const json = res.data;
   return json;
 }
 async function updateApi(id) {
-  const res = await axios.put(`http://localhost:5000/ecosolys/${id}`, {
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-    },
-  });
+  const res = await axios.put(
+    `https://badjokes-server.herokuapp.com/ecosolys${id}`,
+    {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+      },
+    }
+  );
   console.log(res);
   const json = res.data;
   console.log(json);
