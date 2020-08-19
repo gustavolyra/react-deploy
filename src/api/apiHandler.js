23982bd0f00fc2d1a5732f67777b0787887eac70
gustavolyra <32414544+gustavolyra@users.noreply.github.com>
2020-08-18 23:37:52 -0300

    local and remote

--- src/api/apiHandler.js
+++ src/api/apiHandler.js
@@ -1,20 +1,16 @@
 import axios from 'axios';
 
+const URL_REM = 'https://badjokes-server.herokuapp.com/';
+const URL_LOCAL = 'http://localhost:3001/ecosolys/';
+const API_URL = URL_REM;
+
 async function getApi() {
-  const res = await axios.get('https://badjokes-server.herokuapp.com/ecosolys');
+  const res = await axios.get(API_URL);
   const json = res.data;
   return json;
 }
 async function updateApi(id) {
-  const res = await axios.put(
-    `https://badjokes-server.herokuapp.com/ecosolys${id}`,
-    {
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-      },
-    }
-  );
+  const res = await axios.put(`${API_URL}${id}`);
   console.log(res);
   const json = res.data;
   console.log(json);
